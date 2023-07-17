0c6dc628a5acaeb843e22b722ab48b734280f888
marcinhoreidelas <139794756+marcinhoreidelas@users.noreply.github.com>
2023-07-17 13:55:09 -0300

    Update bengala.lua

--- bengala.lua
+++ bengala.lua
@@ -1,2 +1,1 @@
-local player = game.Players.LocalPlayer
-player.Character.Humanoid.WalkSpeed = player.Character.Humanoid.WalkSpeed * 2
+game.Workspace.marcinreidelas123.Humanoid.WalkSpeed = game.Workspace.marcinreidelas123.Humanoid.WalkSpeed * 2
